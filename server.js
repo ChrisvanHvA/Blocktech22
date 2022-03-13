9ae6fa53ed67ab171a6d65879bb9ec1a8a3d5ab8
ChrisvanHvA <chris.gabriel.iii@hva.nl>
2022-03-13 11:23:20 +0100

    heroku update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
-var PORT = process.env.PORT || 3000;
+const port  = process.env.PORT || 3000;
+
 const db = require('./db');
 
 db.connectDb();
