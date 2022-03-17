4bbb4cea11bfabd9086363d5bd6d88c08d99dcdf
ChrisvanHvA <chris.gabriel.iii@hva.nl>
2022-03-17 22:50:09 +0100

    herokutest

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const { MongoClient } = require("mongodb");
-// Replace the uri string with your MongoDB deployment's connection string.
-const uri =
-  "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority";
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    await client.connect();
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+var PORT = process.env.PORT || 5000;
+var express = require('express');
+var app = epress();
+
+var http = require('http');
+var server = http.server(app);
+
+app.use(express.static('client'));
+
+server.listen(PORT, function() {
+  console.log('the server is running');
+  
+});
+
+var io = require('socket.io')(server);
+
